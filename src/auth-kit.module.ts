1c674fbbe111f4da12a5b6740ca1da07133adde7
Zaiidmo <zaiidmoumnii@gmail.com>
2026-01-21 16:59:16 +0100

    refactor: update authKit Module to not ever call any db, and delete client model

--- src/auth-kit.module.ts
+++ src/auth-kit.module.ts
@@ -1,10 +1,8 @@
 ﻿import 'dotenv/config';
-import { MiddlewareConsumer, Module, NestModule, OnModuleDestroy, OnModuleInit, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import passport from './config/passport.config';
 import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
 
-import { connectDB } from './config/db.config';
 import { AuthController } from './controllers/auth.controller';
 import { PasswordResetController } from './controllers/password-reset.controller';
 import { UsersController } from './controllers/users.controller';
@@ -22,15 +20,7 @@ import { AdminController } from './controllers/admin.controller';
     AdminController,
   ],
 })
-export class AuthKitModule implements NestModule, OnModuleInit, OnModuleDestroy {
-  async onModuleInit(): Promise<void> {
-    await connectDB();
-  }
-
-  async onModuleDestroy(): Promise<void> {
-    await mongoose.disconnect();
-  }
-
+export class AuthKitModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(cookieParser(), passport.initialize())

b18bf55b08dd058b1f78586b6634aea33d02d06e
Zaiidmo <zaiidmoumnii@gmail.com>
2026-01-22 10:59:09 +0100

    refactor: Update the authentication guard, and wiring new implementations into Kit module

--- src/auth-kit.module.ts
+++ src/auth-kit.module.ts
@@ -1,29 +1,53 @@
 ﻿import 'dotenv/config';
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import passport from './config/passport.config';
+import { MongooseModule } from '@nestjs/mongoose';
 import cookieParser from 'cookie-parser';
 
-import { AuthController } from './controllers/auth.controller';
-import { PasswordResetController } from './controllers/password-reset.controller';
-import { UsersController } from './controllers/users.controller';
-import { RolesController } from './controllers/roles.controller';
-import { PermissionsController } from './controllers/permissions.controller';
-import { AdminController } from './controllers/admin.controller';
+import { AuthController } from '@controllers/auth.controller';
+import { UsersController } from '@controllers/users.controller';
+import { RolesController } from '@controllers/roles.controller';
+import { PermissionsController } from '@controllers/permissions.controller';
+
+import { User, UserSchema } from '@models/user.model';
+import { Role, RoleSchema } from '@models/role.model';
+import { Permission, PermissionSchema } from '@models/permission.model';
+
+import { AuthService } from '@services/auth.service';
+import { MailService } from '@services/mail.service';
+import { UserRepository } from '@repos/user.repository';
+
+import { AuthenticateGuard } from '@middleware/authenticate.guard';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Role.name, schema: RoleSchema },
+      { name: Permission.name, schema: PermissionSchema },
+    ]),
+  ],
   controllers: [
     AuthController,
-    PasswordResetController,
     UsersController,
     RolesController,
     PermissionsController,
-    AdminController,
+  ],
+  providers: [
+    AuthService,
+    MailService,
+    UserRepository,
+    AuthenticateGuard,
+  ],
+  exports: [
+    AuthenticateGuard,
+    AuthService,
+    UserRepository,
   ],
 })
 export class AuthKitModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(cookieParser(), passport.initialize())
+      .apply(cookieParser())
       .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }

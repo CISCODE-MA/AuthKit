8c848416b84127d27539223167b8f2b9a2f17490
Zaiidmo <zaiidmoumnii@gmail.com>
2026-03-02 09:43:24 +0000

    chore(tests): lowered coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -26,10 +26,10 @@ const config: Config = {
   coverageDirectory: "coverage",
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
 };
